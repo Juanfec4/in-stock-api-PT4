f006c77e1ce87ffee5c78ba7fcdf66ffdfad6288
Lester Blake <lstr.mail@gmail.com>
2023-10-06 17:06:38 -0400

    cleaning up file structure

--- src/routes/warehousesRoutes.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import warehousesController from '../controllers/warehousesController';
-
-router.put('/api/warehouses/:id', warehousesController.update);
-
-export default router;
