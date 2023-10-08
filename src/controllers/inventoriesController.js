e4440f6af6d739372d86fde08e3dd8befc20dec4
Lester Blake <lstr.mail@gmail.com>
2023-10-08 11:00:11 -0400

    completed API for GET single Inv item. Cannot test due to mySQL auth issues.

--- src/controllers/inventoriesController.js
+++ src/controllers/inventoriesController.js
@@ -27,4 +27,47 @@ const index = async (req, res) => {
     }
 };
 
-export default { index }; 
+const getSingleItem = async (req, res) => {
+    //Check for id param
+    if (!req?.params?.id) {
+      return res.status(400).json({ message: "Missing id param." });
+    }
+  
+    //Extract specific query
+    let query = { 'inventories.id': req.params.id };
+  
+    try {
+      //Get Inventory item details
+      const inventory = await knex('inventories')
+        .join('warehouses', 'inventories.warehouse_id', '=', 'warehouses.id')
+        .select(
+          'inventories.id',
+          'warehouses.warehouse_name',
+          'inventories.item_name',
+          'inventories.description',
+          'inventories.category',
+          'inventories.status',
+          'inventories.quantity'
+        )
+        .where(query);
+  
+      //Check if results are 0 (no inventory found)
+      if (inventory?.length === 0) {
+        return res.status(404).json({ message: `No inventory found with id ${req.params.id}` });
+      }
+  
+      //Convert result (Row packet into JSON object)
+      let result = rowDataToJson(inventory[0]); 
+  
+      //Return result
+      return res.status(200).json(result);
+    } catch (e) {
+      return res.status(500).json({ message: e });
+    }
+  };
+  
+
+export default { 
+    index,
+    getSingleItem,
+}; 
--- src/routes/inventoriesRoutes.mjs
+++ src/routes/inventoriesRoutes.mjs
@@ -2,6 +2,14 @@ import express from 'express';
 const router = express.Router();
 import inventoriesController from '../controllers/inventoriesController.js';
 
-router.get('/inventories', inventoriesController.index);
+//Get all inventories
+router.get("/inventories", (req, res) => {
+    return inventoriesController.index(req, res);
+  });
+
+//Get single Inventory
+router.get("/inventories/:id", (req, res) => {
+    return inventoriesController.getSingleItem(req, res)
+})
 
 export default router;
